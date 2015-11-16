1f350a09ca1a85166459f5a3a63474f9cfb11297
Tristan Villanueva <tan_vil@hotmail.com>
2015-11-16 17:40:30 -0500

    Added images and image links, added application functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116202219) do
+ActiveRecord::Schema.define(version: 20151116215035) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email"
     t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "homes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
