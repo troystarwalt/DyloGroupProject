48a9b4f49d17250cb8c845ff9a867a49858056cc
Troy Starwalt <troy.starwalt@gmail.com>
2016-01-13 13:37:17 -0500

    Updated Nav and Alignment

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -9,3 +9,20 @@ $(document).ready(function(){
 $(document).ready(function(){
 	$("#catchphrase1").fadeIn(1500);
 });
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+$('body').scrollspy({ target: '#navbar' })
